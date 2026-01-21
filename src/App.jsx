5924897829274dccd8911ef374f31192803aab6e
Ethanfaria <ethanfaria14@gmail.com>
2026-01-21 09:40:33 +0530

    Initial commit: Setup React with Tailwind and added basic pages

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import BrowseGrounds from './pages/BrowseGrounds'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/grounds" element={<BrowseGrounds/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/BrowseGrounds.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BrowseGrounds = () => {
+  return (
+    <div>BrowseGrounds</div>
+  )
+}
+
+export default BrowseGrounds
