284905a0c1c7838c3482c7f419e5c9b51f87a01f
Ethanfaria <ethanfaria14@gmail.com>
2026-01-30 18:35:39 +0530

    Added Constructors

--- gameday-backend/src/main/java/com/gameday/backend/model/User.java
+++ gameday-backend/src/main/java/com/gameday/backend/model/User.java
@@ -1,13 +1,41 @@
 package com.gameday.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "users")
 public class User {
-    private int userId;
+    @Id
+    @Column(name = "user_id", length=16)
+    private String userId;
+    @Column(unique = true, nullable = false)
     private String email;
     private String name;
     private String phone;
+    @Column(name = "password_hash")
     private String passwordHash;
-    private Role role = Role.PLAYER;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    public User(){}
+
+    public User(String userId, String email, String name, String phone, String passwordHash) {
+        this.userId = userId;
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+        this.passwordHash = passwordHash;
+        this.role = Role.PLAYER;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
 }
