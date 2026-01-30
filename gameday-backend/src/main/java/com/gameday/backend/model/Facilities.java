06038cdfe91ebed947a08f351e19db959885089e
Ethanfaria <ethanfaria14@gmail.com>
2026-01-30 18:36:49 +0530

    Refactored Facilities to Facility

--- gameday-backend/src/main/java/com/gameday/backend/model/Facilities.java
+++
@@ -1,8 +0,0 @@
-package com.gameday.backend.model;
-
-public class Facilities {
-    private int facility_id;
-    private String facility_name;
-    private String location;
-    private User owner;
-}
