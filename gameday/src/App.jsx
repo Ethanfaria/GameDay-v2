34312de3611eef39ed4bda29829f79eef1c7eba0
Ethanfaria <ethanfaria14@gmail.com>
2026-02-20 19:02:43 +0530

    Added routing for few pages

--- gameday/src/App.jsx
+++ gameday/src/App.jsx
@@ -5,6 +5,8 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import BrowseFacilities from "./pages/BrowseFacilities.jsx";
+import Placeholder from "./pages/Placeholder.jsx";
+import FacilityDetails from "./pages/FacilityDetails.jsx";
 
 const App = () => {
   return (
@@ -15,6 +17,11 @@ const App = () => {
         <Route path="/register" element={<Register/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/facilities" element={<BrowseFacilities/>} />
+          <Route path="/academy" element={<Placeholder title="Academy" />} />
+          <Route path="/tournament" element={<Placeholder title="Tournament" />} />
+          <Route path="/about" element={<Placeholder title="About" />} />
+          <Route path="/faq" element={<Placeholder title="FAQ" />} />
+          <Route path="/facilities/:id" element={<FacilityDetails/>}/>
       </Routes>
     </BrowserRouter>
   )
