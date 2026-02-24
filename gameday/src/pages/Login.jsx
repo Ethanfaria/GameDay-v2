3df86ebd44b0bf6435521d4b2c7b4d3efed9aa9b
Ethanfaria <ethanfaria14@gmail.com>
2026-02-24 16:25:08 +0530

    Added form handling for login

--- gameday/src/pages/Login.jsx
+++ gameday/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from '../components/Navbar'
 import BentoBox from "../components/BentoBox.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,46 +9,87 @@ import {
     faLock,
     faEye
 } from "@fortawesome/free-solid-svg-icons";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 const Login = () => {
+  const [formData, setFormData] = useState({email:'',password:''});
+  const [loading, setLoading] = useState(false);
+  const [error, setError]=useState(null);
+  const navigate=useNavigate();
+
+  const handleChange=(e)=>{
+    e.preventDefault();
+    setFormData({
+      ...formData, [e.target.name]:e.target.value
+    });
+  };
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    setError(null);
+    try{
+      setLoading(true);
+      const response=await axios.post("http://localhost:8080/login",formData);
+      localStorage.setItem("token",response.data.token);
+      localStorage.setItem("user",JSON.stringify({
+        userId:response.data.userId,
+        name:response.data.name,
+        email:response.data.email
+      }));
+      navigate("/")
+    }catch(err){
+      setError(err.response?.data||"Login Failed")
+    }finally {
+      setLoading(false);
+    }
+  }
   return (
     <div className="min-h-screen bg-green-950 text-white px-4 md:px-6">
       <Navbar />
       <div className="flex justify-center items-center p-5">
-        <BentoBox className="bg-white/10 p-10 rounded-xl flex flex-col items-center gap-4 w-full max-w-lg">
-          <FontAwesomeIcon className="text-lime-400 text-7xl " icon={faUserShield} />
-          <h1 className="text-lime-400 text-3xl font-bold uppercase" >Welcome Back</h1>
-          <p className="text-gray-400">Login to your account</p>
-          <div className="relative w-full mt-5">
-            <FontAwesomeIcon
-                icon={faEnvelope}
-                className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
-            />
+        <BentoBox className="bg-white/10 p-10 rounded-xl gap-4 w-full max-w-lg">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-center">
+            <FontAwesomeIcon className="text-lime-400 text-7xl " icon={faUserShield} />
+            <h1 className="text-lime-400 text-3xl font-bold uppercase text-center" >Welcome Back</h1>
+            <p className="text-gray-400">Login to your account</p>
+            {error && <p className="text-red-400 text-sm">{error}</p>}
+            <div className="relative w-full mt-5">
+              <FontAwesomeIcon
+                  icon={faEnvelope}
+                  className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
+              />
+              <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Enter your Email"
+                  className="w-full p-3 pl-12 rounded-xl bg-white text-black placeholder:text-gray-400"
+              />
+            </div>
 
-            <input
-                type="email"
-                placeholder="Enter your Email"
-                className="w-full p-3 pl-12 rounded-xl bg-white text-black placeholder:text-gray-400"
-            />
-          </div>
+            <div className="relative w-full mt-5">
+              <FontAwesomeIcon
+                  icon={faLock}
+                  className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
+              />
 
-          <div className="relative w-full mt-5">
-            <FontAwesomeIcon
-                icon={faLock}
-                className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
-            />
-
-            <input
-                type="password"
-                placeholder="Enter your Password"
-                className="w-full p-3 pl-12 rounded-xl bg-white text-black placeholder:text-gray-400"
-            />
-            <FontAwesomeIcon
-                icon={faEye}
-                className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-600"
-            />
-          </div>
-          <button className="mt-5 w-full py-3 bg-lime-400 text-green-900 rounded-xl font-bold hover:scale-105 transition">Login to GameDay</button>
-          <p className="mt-5 text-center text-gray-400 ">Don't have an account? <a href="/register" className="text-lime-400 hover:underline">Register here</a></p>
+              <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="Enter your Password"
+                  className="w-full p-3 pl-12 rounded-xl bg-white text-black placeholder:text-gray-400"
+              />
+              <FontAwesomeIcon
+                  icon={faEye}
+                  className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-600"
+              />
+            </div>
+            <button type="submit" disabled={loading} className="mt-5 w-full py-3 bg-lime-400 text-green-900 rounded-xl font-bold hover:scale-105 transition">{loading?"loading":"Login to GameDay"}</button>
+            <p className="mt-5 text-center text-gray-400 ">Don't have an account? <a href="/register" className="text-lime-400 hover:underline">Register here</a></p>
+          </form>
         </BentoBox>
       </div>
     </div>
--- gameday/src/pages/Register.jsx
+++ gameday/src/pages/Register.jsx
@@ -2,8 +2,9 @@ import React,{useState} from 'react'
 import NavBar from "../components/NavBar.jsx";
 import BentoBox from "../components/BentoBox.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faEnvelope, faEye, faLock, faPhone, faUser} from "@fortawesome/free-solid-svg-icons";
+import {faEnvelope, faEye, faLock, faPhone, faUser, faUserShield} from "@fortawesome/free-solid-svg-icons";
 import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -12,87 +13,131 @@ const Register = () => {
         phone: '',
         password: '',
         confirmPassword: ''
-    })
+    });
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-
+    const handleChange = (e)=>{
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
     const handleSubmit = async (e) => {
-
+        e.preventDefault();
+        setError(null);
+        if(formData.password != formData.confirmPassword){
+            return setError("Passwords do not match");
+        }
+        try {
+            setLoading(true);
+            await axios.post("http://localhost:8080/register",{
+                name: formData.name,
+                email: formData.email,
+                phone: formData.phone,
+                password: formData.password
+            });
+            alert("Registration Successful");
+            navigate("/login");
+        }catch(err){
+            setError(err.response?.data||"Registration Failed");
+        }finally{
+            setLoading(false);
+        }
     }
 
   return (
     <div className="min-h-screen bg-green-950 text-white px-4 md:px-6">
       <NavBar/>
       <div className="flex justify-center items-center p-5">
-          <BentoBox className="bg-white/10 p-10 rounded-xl flex flex-col items-center gap-4 w-full max-w-lg">
-              <h1 className="text-lime-400 text-3xl font-bold uppercase">Welcome to GameDay</h1>
-              <p className="text-gray-400">Register to start booking your slots</p>
-              <div className="relative w-full mt-5">
-                  <FontAwesomeIcon
-                      icon={faUser}
-                      className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
-                  />
-                  <input
-                      type="text"
-                      placeholder="Enter a Username"
-                      className="w-full p-3 pl-12 rounded-xl bg-white text-black placeholder:text-gray-400"/>
-              </div>
-              <div className="relative w-full mt-5">
-                  <FontAwesomeIcon
-                      icon={faEnvelope}
-                      className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
-                  />
-
-                  <input
-                      type="email"
-                      placeholder="Enter your Email"
-                      className="w-full p-3 pl-12 rounded-xl bg-white text-black placeholder:text-gray-400"
-                  />
-              </div>
-              <div className="relative w-full mt-5">
-                  <FontAwesomeIcon
-                      icon={faPhone}
-                      className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
-                  />
-                  <input
-                      type="tel"
-                      placeholder="Enter your Phone Number"
-                      className="w-full p-3 pl-12 rounded-xl bg-white text-black placeholder:text-gray-400"
-                  />
-              </div>
-              <div className="relative w-full mt-5">
-                  <FontAwesomeIcon
-                      icon={faLock}
-                      className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
-                  />
-
-                  <input
-                      type="password"
-                      placeholder="Enter your Password"
-                      className="w-full p-3 pl-12 rounded-xl bg-white text-black placeholder:text-gray-400"
-                  />
-                  <FontAwesomeIcon
-                      icon={faEye}
-                      className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-600"
-                  />
-              </div>
-              <div className="relative w-full mt-5">
-                  <FontAwesomeIcon
-                      icon={faLock}
-                      className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
-                  />
-
-                  <input
-                      type="password"
-                      placeholder="Re Enter your Password"
-                      className="w-full p-3 pl-12 rounded-xl bg-white text-black placeholder:text-gray-400"
-                  />
-                  <FontAwesomeIcon
-                      icon={faEye}
-                      className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-600"
-                  />
-              </div>
+          <BentoBox className="bg-white/10 p-7 rounded-xl  w-full max-w-lg">
+              <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-center">
+                  <FontAwesomeIcon className="text-lime-400 text-7xl " icon={faUserShield} />
+                  <h1 className="text-lime-400 text-3xl font-bold uppercase text-center">Welcome to GameDay</h1>
+                  <p className="text-gray-400">Register to start booking your slots</p>
+                  <div className="relative w-full mt-5">
+                      <FontAwesomeIcon
+                          icon={faUser}
+                          className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
+                      />
+                      <input
+                          type="text"
+                          name="name"
+                          value={formData.name}
+                          onChange={handleChange}
+                          placeholder="Enter a Username"
+                          className="w-full p-3 pl-12 rounded-xl bg-white text-black"
+                      />
+                  </div>
+                  <div className="relative w-full mt-5">
+                      <FontAwesomeIcon
+                          icon={faEnvelope}
+                          className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
+                      />
+                      <input
+                          type="email"
+                          name="email"
+                          value={formData.email}
+                          onChange={handleChange}
+                          placeholder="Enter your Email"
+                          className="w-full p-3 pl-12 rounded-xl bg-white text-black"
+                      />
+                  </div>
+                  <div className="relative w-full mt-5">
+                      <FontAwesomeIcon
+                          icon={faPhone}
+                          className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
+                      />
+                      <input
+                          type="tel"
+                          name="phone"
+                          value={formData.phone}
+                          onChange={handleChange}
+                          placeholder="Enter your Phone Number"
+                          className="w-full p-3 pl-12 rounded-xl bg-white text-black"
+                      />
+                  </div>
+                  <div className="relative w-full mt-5">
+                      <FontAwesomeIcon
+                          icon={faLock}
+                          className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
+                      />
+                      <input
+                          type="password"
+                          name="password"
+                          value={formData.password}
+                          onChange={handleChange}
+                          placeholder="Enter your Password"
+                          className="w-full p-3 pl-12 rounded-xl bg-white text-black"
+                      />
+                      <FontAwesomeIcon
+                          icon={faEye}
+                          className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-600"
+                      />
+                  </div>
+                  <div className="relative w-full mt-5">
+                      <FontAwesomeIcon
+                          icon={faLock}
+                          className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600"
+                      />
+                      <input
+                          type="password"
+                          name="confirmPassword"
+                          value={formData.confirmPassword}
+                          onChange={handleChange}
+                          placeholder="Re Enter your Password"
+                          className="w-full p-3 pl-12 rounded-xl bg-white text-black"
+                      />
+                      <FontAwesomeIcon
+                          icon={faEye}
+                          className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-600"
+                      />
+                  </div>
+                  <button
+                      type="submit"
+                      disabled={loading}
+                      className="mt-5 w-full py-3 bg-lime-400 text-green-900 rounded-xl font-bold hover:scale-105 transition">{loading ? "Creating an Account..." : "Sign up to GameDay"}</button>
+              </form>
           </BentoBox>
       </div>
     </div>
