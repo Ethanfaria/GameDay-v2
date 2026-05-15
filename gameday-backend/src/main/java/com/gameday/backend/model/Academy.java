b595fedb9a6edfed85231a7ee54b47bd2e19b117
Ethanfaria <ethanfaria14@gmail.com>
2026-05-15 16:58:09 +0530

    add: getters and setters

--- gameday-backend/src/main/java/com/gameday/backend/model/Academy.java
+++ gameday-backend/src/main/java/com/gameday/backend/model/Academy.java
@@ -1,11 +1,14 @@
 package com.gameday.backend.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-
+@Getter
+@Setter
 @Entity
 @Table(name = "academies")
 public class Academy {
