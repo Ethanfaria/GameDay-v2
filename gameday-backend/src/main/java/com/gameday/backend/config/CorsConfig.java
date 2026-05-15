f96786733d768de936c27e1b5fb87409b9b2ee48
Ethanfaria <ethanfaria14@gmail.com>
2026-05-15 16:39:19 +0530

    add default Green BG

--- gameday-backend/src/main/java/com/gameday/backend/config/CorsConfig.java
+++ gameday-backend/src/main/java/com/gameday/backend/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:3000","http://192.168.1.5:3000","http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
